1289bbd3acd857cd22209a61d2a50c50de40c803
saracj <s.c.jonvik@astro.uio.no>
2014-09-22 15:40:13 +0200

    Removed print commands

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -16,7 +16,7 @@ int main(){
     double rho_max = 10.;
     double rho_min = 0.;
     double h = (rho_max - rho_min)/N;
-    double omega_r = 5.;
+    double omega_r = 0.01;
     double TIME1, TIME2;
     vec rho(n), V(n-2), eigval;
     mat A = zeros<mat>(n-2,n-2);
@@ -26,7 +26,7 @@ int main(){
     clock_t start, finish, start2, finish2; // Declare start and finish time
     for(int i=1; i<n-1; i++){
         rho[i] = rho_min + (i+1)*h;
-        V[i] = omega_r*rho[i]*rho[i] + 1./rho[i];
+        V[i] = omega_r*omega_r*rho[i]*rho[i] + 1./rho[i];
         }
     rho[0] = 0.;
     rho[n-1] = rho_max;
@@ -71,6 +71,7 @@ int main(){
     for(int i=1; i<n-1; i++){
         wave(i) = u_1(i-1);
     }
+
     wave(0) = 0;
     wave(n-1) = 0;
 
@@ -79,22 +80,13 @@ int main(){
     string Omega = static_cast<ostringstream*>( \
                 &(ostringstream() << omega_r*100) )->str();
     filename = "wavefunc_omega"+Omega+".dat";
+
     ofile.open(filename.c_str(), ios::out);
     for(int i=0; i<n; i++){ ofile << rho[i] << ' ' << wave[i] << endl; }
     ofile.close();
 
-    /*
-    cout << "jacobi.cpp gives: " << endl;
-    lambda.print("lambda = ");
-    cout << "Built in eigenvalue procedure in armadillog gives:" << endl;
-    eigval.print("lambda_armadillo = ");
-
-    cout << "The three first eigenvalues for \omega_r = " << omega_r << " with n = " << n << ":" << endl;
-    cout << lambda(0) << endl;
-    cout << lambda(1) << endl;
-    cout << lambda(2) << endl;
+    cout << "omega_r = " << omega_r << endl;
     cout << "computation time for jacobi.cpp with n = " << n << ": " << TIME1 << " s" << endl;
     cout << "computation time for built in procedure with n = " << n << ": " << TIME2 << " s" << endl;
-    */
     return 0;
 }
