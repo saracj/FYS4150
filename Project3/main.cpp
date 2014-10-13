63a420c94dfd00a75f03e3cb59d698761b01ead7
saracj <s.c.jonvik@astro.uio.no>
2014-10-13 15:15:54 +0200

    restructured EVERYTHING

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -8,7 +8,12 @@ int main(){
     double  dt = ; // time step
     M_sun = 1.; // solar mass
     M_earth = 1e-6; // earth mass
+
     // Initial velocity (y-direction)
     v = sqrt(G*M_sun/r);
-    celestialbodies earth(M, (1,0), (0,0), dt); //position and velocity of earth
+    celestialbodies sun('Sun', M_sun, (0,0), (0,0));
+    celestialbodies earth('Earth', M_earth, (1,0), (0,0));
 }
+
+
+
