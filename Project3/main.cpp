23ab09994790157537b7be09adbda91b1167d9e3
saracj <s.c.jonvik@astro.uio.no>
2014-10-14 17:26:47 +0200

    Corrected the input from the advance function so that the returned vector is split up and saved into position and

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -14,8 +14,11 @@ int main(){
     double n = T/dt;
     double vx = G*1./1.; // vr^2 = G*M_sun
     double vy = 0;
+    int i;
     vec pos_earth(2), vel_earth(2), pos_sun(2), vel_sun(2), time(n);
-    vec NEXT;
+    vec NEXT_earth(4);
+    mat earth_pos = zeros<mat>(n, 2);
+    mat earth_vel = zeros<mat>(n, 2);
 
     // Initial conditions, earth:
     vel_earth(0) = vx; vel_earth(1) = vy; pos_earth(0) = 0; pos_earth(1) = r;
@@ -30,14 +33,13 @@ int main(){
     MySolarsystem.AddObject(sun);
     MySolarsystem.AddObject(earth);
 
-    int i = 0;
-    // mat earth_pos = zeros<mat>(n, 2);
-    // mat earth_vel = zeros<mat>(n, 2);
-    int t = 0;
-    while(t < T){
-        NEXT = MySolarsystem.advance(earth);
+    for(i=0; i<n-1; i++){
+        NEXT_earth = MySolarsystem.advance(earth); // (x, y, vx, vy)
+        earth_pos(i, 0) = NEXT_earth(0);
+        earth_pos(i, 1) = NEXT_earth(1);
+        earth_vel(i, 0) = NEXT_earth(2);
+        earth_vel(i, 1) = NEXT_earth(3);
         time[i+1] = time[i] + dt;
-        t += dt;
-        i += 1;
     }
+    return 1;
 }
