110c01b23bde8abbcc38f22db366c8c3d4f86f8e
saracj <s.c.jonvik@astro.uio.no>
2014-10-16 18:21:09 +0200

    RK4 do not return anything, takes in the time step. set the positions using the .setPos() function in celestialbodies

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <armadillo>
+#include <cmath>
 #include "solarsystem.h"
 #include "celestialbodies.h"
 
@@ -7,16 +8,15 @@ using namespace std;
 using namespace arma;
 
 int main(){
-    double dt = 1e-2; // time step, [years]
     double r = 1.; // Distance from earth to sun [AU]
-    double G = 1.2e-21; // Gravitational constant [Au^3 / yr^2 M_sun]
-    double T = 2.;
-    double n = T/dt;
-    double vx = 1.; // vr^2 = G*M_sun
+    double G = 4*M_PI*M_PI; // Gravitational constant [Au^3 yr^-2 M_sun^-1]
+    double n = 50.; // number of steps
+    double T = 2.; // end time of simulation [years]
+    double dt = T/n; // time step, [years]
+    double vx = sqrt(G); // v^2 * r = G*M_sun -- v = sqrt(G*M_sun/r)
     double vy = 0;
     int i;
-    vec pos_earth(2), vel_earth(2), pos_sun(2), vel_sun(2);
-    vec NEXT_earth(4);
+    vec pos_earth(2), vel_earth(2), pos_sun(2), vel_sun(2), RK4_earth(2);
     vec time = zeros<vec>(n);
     mat earth_pos = zeros<mat>(n, 2);
     // mat earth_vel = zeros<mat>(n, 2);
@@ -26,19 +26,20 @@ int main(){
     // Initial conditions, Sun:
     vel_sun(0) = 0; vel_sun(1) = 0; pos_sun(0) = 0; pos_sun(1) = 0;
 
-    solarsystem MySolarsystem;
+    solarsystem MySolarsystem(dt);
     // object(string name, mass, position, velocity)
     celestialbodies sun("Sun", 1., pos_sun, vel_sun);
     celestialbodies earth("Earth", 1e-6, pos_earth, vel_earth);
 
     MySolarsystem.AddObject(sun);
     MySolarsystem.AddObject(earth);
-
-    for(i=1; i<n-1; i++){
-        NEXT_earth = MySolarsystem.RK4(earth); // (x, y, vx, vy)
-        earth_pos(i, 0) = NEXT_earth(0);
-        earth_pos(i, 1) = NEXT_earth(1);
+    earth_pos.row(0) = pos_earth.t();
+    for(i=0; i<n-1; i++){
+        MySolarsystem.RK4(); // (x, y), RK4_earth = earth.getPos()
+        cout << earth.getPos() << endl;
+        earth_pos.row(i+1) = earth.getPos().t();// RK4_earth is calculated as a column vector
         time(i+1) = time(i) + dt;
     }
+    cout << "Position vector of earth" << endl << earth_pos << endl;
     return 0;
 }
