0c620822d5af390f6b1f117e8885b3cc614fab6c
saracj <s.c.jonvik@astro.uio.no>
2014-10-16 15:21:29 +0200

    .advance is now .RK4.

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -7,18 +7,19 @@ using namespace std;
 using namespace arma;
 
 int main(){
-    double dt = 1e-3; // time step, [years]
+    double dt = 1e-2; // time step, [years]
     double r = 1.; // Distance from earth to sun [AU]
     double G = 1.2e-21; // Gravitational constant [Au^3 / yr^2 M_sun]
     double T = 2.;
     double n = T/dt;
-    double vx = G*1./1.; // vr^2 = G*M_sun
+    double vx = 1.; // vr^2 = G*M_sun
     double vy = 0;
     int i;
-    vec pos_earth(2), vel_earth(2), pos_sun(2), vel_sun(2), time(n);
+    vec pos_earth(2), vel_earth(2), pos_sun(2), vel_sun(2);
     vec NEXT_earth(4);
+    vec time = zeros<vec>(n);
     mat earth_pos = zeros<mat>(n, 2);
-    mat earth_vel = zeros<mat>(n, 2);
+    // mat earth_vel = zeros<mat>(n, 2);
 
     // Initial conditions, earth:
     vel_earth(0) = vx; vel_earth(1) = vy; pos_earth(0) = 0; pos_earth(1) = r;
@@ -33,13 +34,13 @@ int main(){
     MySolarsystem.AddObject(sun);
     MySolarsystem.AddObject(earth);
 
-    for(i=0; i<n-1; i++){
-        NEXT_earth = MySolarsystem.advance(earth); // (x, y, vx, vy)
+    for(i=1; i<n-1; i++){
+        cout << "Main for-loop counter = " << i << endl;
+        NEXT_earth = MySolarsystem.RK4(earth); // (x, y, vx, vy)
+        cout << "next-array: " << NEXT_earth << endl;
         earth_pos(i, 0) = NEXT_earth(0);
         earth_pos(i, 1) = NEXT_earth(1);
-        earth_vel(i, 0) = NEXT_earth(2);
-        earth_vel(i, 1) = NEXT_earth(3);
-        time[i+1] = time[i] + dt;
+        time(i+1) = time(i) + dt;
     }
-    return 1;
+    return 0;
 }
