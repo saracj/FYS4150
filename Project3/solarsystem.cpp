bf3204c8c96cdedf0de293b752bb4d40fb4722a5
saracj <s.c.jonvik@astro.uio.no>
2014-10-16 18:19:07 +0200

    Added a loop in the RK4 function in order to generalize to more planets. Changed RK4 to void

--- Project3/solarsystem.cpp
+++ Project3/solarsystem.cpp
@@ -6,7 +6,8 @@
 using namespace arma;
 using namespace std;
 
-solarsystem::solarsystem(){
+solarsystem::solarsystem(double m_dt){
+    this->dt = m_dt;
 }
 
 void solarsystem::AddObject(celestialbodies newobject){
@@ -17,14 +18,16 @@ void solarsystem::AddObject(celestialbodies newobject){
 vec solarsystem::getForces(celestialbodies object){
     vec F_g = zeros<vec>(2);
     for(this->i=0; this->i < getNumberOfObj(); this->i++){
-        if(object.getID() == objects[i].getID()){
-            continue; }
-        R = object.getDist(objects[i]);
-        m = object.getM();
-        M = objects[i].getM();
-        G = 1.2e-21; // Gravitational constant [Au^3 / yr^2 M_sun]
-        F_g += G*M*m*R/pow(norm(R), 3);
+        if(object.getID() == objects[i].getID()){ continue; }
+
+        this->R = object.getDist(objects[i]);
+        this->m = object.getM();
+        this->M = objects[i].getM();
+        this->G = 4*M_PI*M_PI; // Gravitational constant [Au^3 / yr^2 M_sun]
+        this->fg = G*M*m*R/pow(norm(R), 3);
+        F_g += fg;
     }
+    //cout << "Force on " << object.getID() << ": "<< endl << F_g << endl;
     return F_g;
 }
 
@@ -43,47 +46,57 @@ vec solarsystem::verlet(celestialbodies object){
 }
 
 // Advances one object one time step ahead
-vec solarsystem::RK4(celestialbodies object){
+void solarsystem::RK4(){
+    k1 = zeros<vec>(2); k2 = zeros<vec>(2); k3 = zeros<vec>(2); k4 = zeros<vec>(2);
+    K1 = zeros<vec>(2); K2 = zeros<vec>(2); K3 = zeros<vec>(2); K4 = zeros<vec>(2);
+    // Loops over all the objects in the system.
+    for(this->i=0; this->i < getNumberOfObj(); this->i++){
+        if(objects[i].getID() == "Sun"){ continue; } // Keep sun stationary
 
-    // Runge-Kutta, fourth order:
-    RK4_vel = object.getVel();
-    RK4_pos = object.getPos();
-    cout << "initiation of RK complete"<< endl;
+        // Runge-Kutta, fourth order:
+        RK4_vel = objects[i].getVel();
+        RK4_pos = objects[i].getPos();
+        //cout << RK4_vel << endl;
 
-    k1 = acceleration(object);
-    K1 = object.getVel();
-    // This needs to be done between each k because
-    // the force depends on position, meaning
-    // that we need to update the position
-    object.setPos(RK4_pos + 0.5*dt*K1);
-    k2 = acceleration(object);
-    K2 = object.getVel();
+        k1 = acceleration(objects[i]);
+        K1 = objects[i].getVel();
+        cout << K1 << endl;
+        objects[i].setPos(RK4_pos + 0.5*dt*K1);
+        //cout << "Acceleration of object " << objects[i].getID() << ": "<< endl << k1 << endl;
+        //cout << "Distance: " << endl << this->R  << endl;
 
-    object.setPos(RK4_pos + 0.5*dt*K2);
-    k3 = acceleration(object);
-    K3 = object.getVel();
 
-    object.setPos(RK4_pos + 0.5*dt*K3);
-    k4 = acceleration(object);
-    K4 = object.getVel();
+        k2 = acceleration(objects[i]);
+        K2 = K1 + 0.5*dt*k1;
+        objects[i].setPos(RK4_pos + 0.5*dt*K2);
 
-    RK4_next_vel = RK4_vel + (dt/6.)*(k1 + 2*k2 + 2*k3 + k4);
-    RK4_next_pos = RK4_pos + (dt/6.)*(K1 + 2*K2 + 2*K3 + K4);
+        k3 = acceleration(objects[i]);
+        K3 = K1 + 0.5*dt*k2;
+        objects[i].setPos(RK4_pos + 0.5*dt*K3);
 
-    object.setVel(RK4_next_vel);
-    object.setPos(RK4_next_pos);
+        k4 = acceleration(objects[i]);
+        K4 = K1 + 0.5*dt*k3;
 
-    RK4_next = zeros<vec>(4);
-    RK4_next(0) = RK4_next_pos(0);
-    RK4_next(1) = RK4_next_pos(1);
-    RK4_next(2) = RK4_next_vel(0);
-    RK4_next(3) = RK4_next_vel(1);
+        /*
+        cout << "Acceleration to velocity:" << endl;
+        cout << "k1 = " << endl << k1 << endl;
+        cout << "k2 = " << endl << k2 << endl;
+        cout << "k3 = " << endl << k3 << endl;
+        cout << "k4 = " << endl << k4 << endl;
+        cout << "Velocity to position:" << endl;
+        cout << "K1 = " << endl << k1 << endl;
+        cout << "K2 = " << endl << k2 << endl;
+        cout << "K3 = " << endl << k3 << endl;
+        cout << "K4 = " << endl << K4 << endl;
+        */
 
-    object.setF(zeros<vec>(2));
-    return RK4_next;
-    // Saving position and time to arrays
-    // *pos_mat[i+1, i+1] = next_pos;
-    // *vel_mat[i+1, i+1] = next_vel;
+        RK4_next_vel = RK4_vel + (dt/6.)*(k1 + 2*k2 + 2*k3 + k4);
+        RK4_next_pos = RK4_pos + (dt/6.)*(K1 + 2*K2 + 2*K3 + K4);
+        //cout << "Change in position: " << endl << (dt/6.)*(K1 + 2*K2 + 2*K3 + K4) << endl;
+
+        objects[i].setVel(RK4_next_vel);
+        objects[i].setPos(RK4_next_pos);
+    }
 }
 
 int solarsystem::getNumberOfObj(){ return objects.size(); }
