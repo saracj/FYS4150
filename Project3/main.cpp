dc52d1b7e12467c087b043ba1cfcdddc6d8ec0a1
saracj <s.c.jonvik@astro.uio.no>
2014-10-17 20:37:19 +0200

    Created two seperate solar systems, one for the verlet method and one for RK4. Added two strings, one for the time step and one for end time. Import string describing both method, end time and time step in the solarsystem constructor

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -8,45 +8,62 @@ using namespace std;
 using namespace arma;
 
 int main(){
+
+    int j;
+    int n = 10000; // number of steps
+
     double r = 1.; // Distance from earth to sun [AU]
     double G = 4*M_PI*M_PI; // Gravitational constant [Au^3 yr^-2 M_sun^-1]
-    int n = 5000000.; // number of steps
-    double T = 500.; // end time of simulation [years]
+    double T = 5.; // end time of simulation [years]
     double dt = T/n; // time step, [years]
-    double vx = 2*M_PI; // v^2 * r = G*M_sun -- v = sqrt(G*M_sun/r)
+    double vx = sqrt(G); // v^2 * r = G*M_sun -- v = sqrt(G*M_sun/r)
     double vy = 0;
-    int j;
 
     vec pos_earth(2), vel_earth(2), pos_sun(2), vel_sun(2);
-    //vec time = zeros<vec>(n);
-    //mat earth_pos = zeros<mat>(n, 2);
 
     // Initial conditions
     vel_earth(0) = vx; vel_earth(1) = vy; pos_earth(0) = 0; pos_earth(1) = r;
-    vel_sun(0) = 0; vel_sun(1) = 0; pos_sun(0) = 0; pos_sun(1) = 0;
+    vel_sun.fill(0); pos_sun.fill(0);
 
-    solarsystem MySolarsystem(dt);
+    string dt_string = static_cast<ostringstream*>(&(ostringstream() << dt) )->str();
+    string time_string = static_cast<ostringstream*>(&(ostringstream() << T) )->str();
     celestialbodies sun("Sun", 1., pos_sun, vel_sun);
     celestialbodies earth("Earth", 3e-6, pos_earth, vel_earth);
 
-    MySolarsystem.AddObject(&sun);
-    MySolarsystem.AddObject(&earth);
 
-    //earth_pos.row(0) = pos_earth.t();
+    solarsystem MySolarsystem_verlet(dt, "verlet_"+time_string+"yr_"+dt_string);
+    solarsystem MySolarsystem_RK4(dt, "RK4_"+time_string+"yr_"+dt_string);
+
+    MySolarsystem_verlet.AddObject(&sun);
+    MySolarsystem_verlet.AddObject(&earth);
+
+    MySolarsystem_RK4.AddObject(&sun);
+    MySolarsystem_RK4.AddObject(&earth);
+
     for(j=0; j<n-1; j++){
 
         // Print progress.
-        if (j % (n/1000) == 0) {
+        if (j % (n/100) == 0 && n > 1000) {
             printf("Progress: %5.1f %% \r", 100*j/ ((double) n));
             fflush(stdout);
         }
 
-        //MySolarsystem.RK4();
-        MySolarsystem.verlet(j);
-        //earth_pos.row(j+1) = earth.getPos().t();// RK4 is calculated as a column vector
-        //time(j+1) = time(j) + dt;
+        MySolarsystem_verlet.verlet(j);
     }
-    //cout << "Position vector of earth" << endl << earth_pos << endl;
+    cout << "Completed Verlet Procedure" << endl;
+
+    // Fourth order Runge-Kutta:
+    for(j=0; j<n-1; j++){
+
+        // Print progress.
+        if (j % (n/100) == 0 && n > 1000) {
+            printf("Progress: %5.1f %% \r", 100*j/ ((double) n));
+            fflush(stdout);
+        }
+
+        MySolarsystem_RK4.RK4();
+    }
+    cout << "Completed RK4 Procedure" << endl;
     return 0;
 }
 
