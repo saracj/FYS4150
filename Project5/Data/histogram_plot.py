a026aa520ebf779f44693ef0747983b8eecd69d6
saracj <sara.jonvik@gmail.com>
2014-11-26 09:37:26 +0100

    Included the python plotting program

--- Project5/Data/histogram_plot.py
+++ Project5/Data/histogram_plot.py
@@ -18,8 +18,21 @@ def read_file(filename):
   return u
   
 filename_gauss = 'Histogram_gaussStepLength.dat'
-u = read_file(filename_gauss)
+filename_const = 'Histogram_constStepLength.dat'
+u_gauss = read_file(filename_gauss)
+u_const = read_file(filename_const)
 
-plt.hist(u, bins=100, color='blue', histtype='step')
+
+plt.hist(u_const, bins=50, color='green')
+plt.title('Particle concentration (constant step length)')
+plt.xlabel(r'Position $x$')
+plt.ylabel(r'Consentration $u(x)$')
+plt.savefig(filename_const.split('.')[0]+'.png')
+plt.show()
+
+plt.hist(u_gauss, bins=100, color='blue', histtype='step')
+plt.title('Particle concentration (gaussian random step length)')
+plt.xlabel(r'Position $x$')
+plt.ylabel(r'Consentration $u(x)$')
 plt.savefig(filename_gauss.split('.')[0]+'.png')
 plt.show()
